e4e59e05d400bf0cf5e427d7314fe70e0f6aa11d
ummehani <hanirh2468@gmail.com>
2024-11-05 22:45:34 +0500

    portfolio using nextjs

--- src/app/components/card/ProjectCard.tsx
+++ src/app/components/card/ProjectCard.tsx
@@ -1,29 +1,3 @@
-// // import Image from "next/image";
-// const ProjectCard = ({
-//     imageSrc,
-//     imageAlt,
-//     title
-// } : {
-//     imageSrc: string;
-//     imageAlt: string;
-//     title: string
-// }) => {
-//     return (
-//         <div className="flex flex-col items-center gap-4">
-//             <div className="bg-purple-800 p-4 rounded-lg flex flex-col items-center">
-//                 <img
-//                     className="w-48 h-48 object-cover rounded-md"
-//                     src={imageSrc}
-//                     alt={imageAlt}
-//                 />
-//                 <p className="text-white mt-2">{title}</p>
-//             </div>
-//         </div>
-//     );
-// };
-
-// export default ProjectCard;
-
 import Image from "next/image";
 
 const ProjectCard = ({
