6688fb223813a0354ea0078413d462d27dcc7d17
Umm-e-Hani02 <165858620+Umm-e-Hani02@users.noreply.github.com>
2024-11-06 23:24:54 +0500

    Update Footer.tsx

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer(){
     return(
-        <footer className="bg-purple-950 h-10 text-sm items-center justify-center p-10">
+        <footer className="bg-slate-900 h-10 text-sm items-center justify-center p-10 text-gray-200">
             <p className="text-center">© 2024 UMM-E-HANI | All Rights Reserved</p>
         </footer>
     )
