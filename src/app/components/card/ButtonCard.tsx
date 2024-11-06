1f8ee2dec8d2aefe7698a238302715efaa8d752d
Umm-e-Hani02 <165858620+Umm-e-Hani02@users.noreply.github.com>
2024-11-06 23:22:34 +0500

    Update ButtonCard.tsx

--- src/app/components/card/ButtonCard.tsx
+++ src/app/components/card/ButtonCard.tsx
@@ -9,7 +9,7 @@ const ButtonCard = ({
 }) => {
     return (
         <Link href={href} passHref target="_blank">
-            <button className="bg-yellow-600 h-10 w-32 rounded-md mt-7 hover:bg-yellow-700 active:opacity-[0.9] transition-all duration-300">
+            <button className="bg-yellow-600 h-10 w-32 rounded-md mt-7 hover:bg-yellow-700 active:opacity-[0.9] transition-all duration-300 text-gray-200">
                 {text}
             </button>
         </Link>
