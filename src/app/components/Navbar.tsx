910433fca3958e59f789a246ec3001bfa18dcce6
Umm-e-Hani02 <165858620+Umm-e-Hani02@users.noreply.github.com>
2024-11-06 23:25:22 +0500

    Update Navbar.tsx

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -2,8 +2,8 @@ import Link from "next/link";
 
 export default function Navbar() {
     return (
-        <nav className="bg-purple-950 shadow-lg h-16 flex items-center justify-center fixed w-full z-10">
-            <ul className="flex space-x-3 text-white text-sm">
+        <nav className="bg-slate-900 shadow-lg h-16 flex items-center justify-center fixed w-full z-10">
+            <ul className="flex space-x-3 text-gray-200 text-sm">
                 <li>
                     <Link href="#home" className="hover:text-yellow-600 transition duration-300">
                         Home
